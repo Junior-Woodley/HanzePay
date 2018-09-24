9c23d6e3cf66fa2b242f3c4ed7bbae89ae129fcb
Joey <JoeyKhd@users.noreply.github.com>
2018-09-24 12:36:36 +0200

    Added first files and hanzepay logo

---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.10/css/mdb.min.css" rel="stylesheet">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<nav class="navbar navbar-expand-lg navbar-light white mb-3">
+	<a class="navbar-brand" href="#">
+		<img src="./img/hanzepay-logo.png" height="30" alt="">
+	</a>	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+	        aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Features</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Pricing</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#">Disabled</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<div class="container">
+	<div class="col text-center">
+		Uw Balans <br>
+		<h4>340 HP</h4>
+	</div>
+	<div class="row d-flex justify-contents-between">
+		<div class="col-md-6 col-xs-6">
+			<div class="card card-body">
+				<h4 class="card-title">Balans</h4>
+				<p class="card-text">500 Credits</p>
+				<div class="flex-row">
+					<button class="btn btn-primary"><a class="card-link">Credits Overmaken</a></button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
