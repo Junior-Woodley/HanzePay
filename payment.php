07f1059df1e02dcb492cd63f813df52b436df45f
Joey Kheireddine <joey.kheireddine@outlook.com>
2018-10-15 20:01:37 +0200

    Created payment process page for admins

---
+++ payment.php
@@ -0,0 +1,26 @@
+<?php include 'partials/header.php' ?>
+
+    <body>
+    <div class="container mt-3 mb-3">
+        <div class="row">
+            <div class="col-md-12 col-xs-6 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="container col-md-6">
+                        <h5 class="card-title font-weight-bold">Betalingsaanvraag</h5>
+                        <form class="text-center">
+                            <input type="text" id="defaultContactFormName" class="form-control mb-4"
+                                   placeholder="Studentnummer">
+                            <input type="email" id="defaultContactFormEmail" class="form-control mb-4"
+                                   placeholder="Kosten HP Tokens">
+                            <button class="btn btn-hanze btn-rounded btn-block" type="submit">Aan</button>
+                        </form>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+
+<?php include 'partials/footer.php';
